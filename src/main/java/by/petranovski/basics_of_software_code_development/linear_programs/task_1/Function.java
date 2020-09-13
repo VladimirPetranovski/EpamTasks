6dcb6cb6645a60c7bd2400f34954cff8888764d8
VladimirPetranovski <desantluninec@mail.ru>
2020-09-13 22:35:48 +0300

    I'm calculated the value of the function z = z = ((a - 3) * b / 2) + c

---
+++ src/main/java/by/petranovski/basics_of_software_code_development/linear_programs/task_1/Function.java
@@ -0,0 +1,11 @@
+package by.petranovski.basics_of_software_code_development.linear_programs.task_1;
+
+public class Function {
+    static int a = 3, b = 5, c = 7, z;
+
+    public static void main(String[] args) {
+        z = ((a - 3) * b / 2) + c;
+        System.out.println(z);
+    }
+
+}
