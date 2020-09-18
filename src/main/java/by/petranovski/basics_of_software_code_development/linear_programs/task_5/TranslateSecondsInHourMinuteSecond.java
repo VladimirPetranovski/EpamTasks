12fb0647ffc2d047a957fe3ae5ea21c8815d0c19
VladimirPetranovski <desantluninec@mail.ru>
2020-09-18 15:00:26 +0300

    task_5 make in shorter

--- src/main/java/by/petranovski/basics_of_software_code_development/linear_programs/task_5/TranslateSecondsInHourMinuteSecond.java
+++ src/main/java/by/petranovski/basics_of_software_code_development/linear_programs/task_5/TranslateSecondsInHourMinuteSecond.java
@@ -6,10 +6,9 @@ package by.petranovski.basics_of_software_code_development.linear_programs.task_
  */
 public class TranslateSecondsInHourMinuteSecond {
     public static void main(String[] args) {
-        long t = 366388967643424456L;
-        long hour = t / 3600;
-        long min = (t - hour * 3600) / 60;
-        long second = (t - hour * 3600) - min * 60;
-        System.out.println("В натуральном числе " + t + ": " + hour + "ч " + min + "мин " + second + "с.");
+        task(323277718977L);
+    }
+    private static void task(long t) {
+        System.out.println(t / 3600 + "ч " + (t % 3600) / 60 + "м " + (t % 3600) % 60 + "с");
     }
 }
