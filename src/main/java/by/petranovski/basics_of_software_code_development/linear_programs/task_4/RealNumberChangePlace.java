32b4638abb9e63463e17dcdf5142328f2309c0fc
VladimirPetranovski <desantluninec@mail.ru>
2020-09-18 10:38:00 +0300

    rename task_4

--- src/main/java/by/petranovski/basics_of_software_code_development/linear_programs/task_4/RealNumberChangePlace.java
+++ src/main/java/by/petranovski/basics_of_software_code_development/linear_programs/task_4/RealNumberChangePlace.java
@@ -3,7 +3,7 @@ package by.petranovski.basics_of_software_code_development.linear_programs.task_
 Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях). Поменять местами
 дробную и целую части числа и вывести полученное значение числа
  */
-public class RealNumberCheckPlace {
+public class RealNumberChangePlace {
     public static void main(String[] args) {
         double r = 123.456;
         int n = (int)r;
