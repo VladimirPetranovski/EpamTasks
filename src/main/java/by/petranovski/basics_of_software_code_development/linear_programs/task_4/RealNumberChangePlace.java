4e83d06d233cc0ede3f811b0871072cce6add13b
VladimirPetranovski <desantluninec@mail.ru>
2020-09-18 11:02:54 +0300

    Changed place real number 123.456

---
+++ src/main/java/by/petranovski/basics_of_software_code_development/linear_programs/task_4/RealNumberChangePlace.java
@@ -0,0 +1,15 @@
+package by.petranovski.basics_of_software_code_development.linear_programs.task_4;
+/*
+Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой части). Поменять местами
+дробную и целую части числа и вывести полученное значение числа
+*/
+
+public class RealNumberChangePlace {
+    public static void main(String[] args) {
+        double r = 123.456;
+        int n = (int) r;
+        int d = (int) ((r - n) * 1000);
+        double z = (double) d + ((double) n / 1000);
+        System.out.println(z);
+    }
+}
