8b2f730e55f3ad24b3b94ce2db153d480b24c9ce
VladimirPetranovski <desantluninec@mail.ru>
2020-09-19 08:45:14 +0300

    changed task_4, made in shorter

--- src/main/java/by/petranovski/basics_of_software_code_development/linear_programs/task_4/RealNumberChangePlace.java
+++ src/main/java/by/petranovski/basics_of_software_code_development/linear_programs/task_4/RealNumberChangePlace.java
@@ -1,14 +1,15 @@
 package by.petranovski.basics_of_software_code_development.linear_programs.task_4;
+
 /*
 Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях). Поменять местами
 дробную и целую части числа и вывести полученное значение числа
  */
 public class RealNumberChangePlace {
     public static void main(String[] args) {
-        double r = 123.456;
-        int n = (int)r;
-        int d = (int)((r - n) * 1000);
-        double z = (double)d + ((double)n / 1000);
-        System.out.println(z);
+        task(123.456);
+    }
+
+    private static void task(double r) {
+        System.out.println((double) ((int) ((r - (int) r) * 1000)) + (double) ((int) r) / 1000);
     }
 }
